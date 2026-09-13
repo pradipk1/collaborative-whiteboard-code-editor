644ff9534dd92efb990f2994c07882c6214a2fcb
Pradip Shukla <pradipshukla12699@gmail.com>
2026-09-13 13:52:06 +0530

    split functionality added!

--- client/src/components/SplitScreen/index.jsx
+++ client/src/components/SplitScreen/index.jsx
@@ -2,13 +2,58 @@
 import Whiteboard from './../Whiteboard';
 import CodeEditor from './../CodeEditor';
 import './splitScreen.css';
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 function SplitScreen() {
     const [leftWidth, setLeftWidth] = useState(50);
+    const isDragging = useRef(false);
+    const spliScreenContRef = useRef(null);
+    console.log('the component re-rendered!');
+
+    const startResize = (e) => {
+        isDragging.current = true;
+        document.body.style.cursor = 'col-resize';
+        document.body.style.userSelect = 'none'
+    }
+
+    useEffect(() => {
+
+        // handle mouse move event
+        const handleMouseMove = (e) => {
+            if(!isDragging.current || !spliScreenContRef) return;
+
+            const spliScreenContRect = spliScreenContRef.current.getBoundingClientRect();
+            const newLeftWidthPx = e.clientX - spliScreenContRect.left;
+            const newLeftWidthPercentage = (newLeftWidthPx / spliScreenContRect.width) * 100;
+
+            // Constrain layout dimensions (min 20%, max 80%)
+            if(newLeftWidthPercentage >= 20 && newLeftWidthPercentage <= 80) {
+                setLeftWidth(newLeftWidthPercentage);
+            }
+
+
+        }
+
+        const handleMouseUp = () => {
+            if(isDragging.current) {
+                isDragging.current = false;
+                document.body.style.cursor = 'default';
+                document.body.style.userSelect = 'auto';
+            }
+        }
+
+        window.addEventListener('mousemove', handleMouseMove);
+        window.addEventListener('mouseup', handleMouseUp);
+
+        return () => {
+            window.removeEventListener('mousemove', handleMouseMove);
+            window.removeEventListener('mouseup', handleMouseUp)
+        }
+    },
+    []);
 
     return (
-        <div className="splitScreen-cont">
+        <div className="splitScreen-cont" ref={spliScreenContRef}>
             <div className="split-cont">
 
                 {/* left split box */}
@@ -20,7 +65,11 @@ function SplitScreen() {
 
                 {/* screen divider */}
                 <div className="screenDivider"
-                    
+                    onMouseDown={startResize}
+                    onMouseEnter={(e) => {e.target.style.backgroundColor = '#007acc'}}
+                    onMouseLeave={(e) => {
+                        if(!isDragging.current) e.target.style.backgroundColor = '#333';
+                    }}
                 ></div>
 
                 {/* right split box */}
